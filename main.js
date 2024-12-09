550469cde95d9caf2a0eb3b5fd491d4486a21f5d
maxedit30044@gmail.com <maxedit30044@gmail.com>
2024-12-09 12:36:05 +0500

    potfolio

---
+++ main.js
@@ -0,0 +1,5 @@
+       // JavaScript to handle form submission and show the alert
+       document.getElementById('hireMeForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevents the form from submitting
+        alert('Thank you for hiring me!');
+    });
